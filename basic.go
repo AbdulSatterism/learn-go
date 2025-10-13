8c70cc8b40c1a12c8390469197d21afaff0e6e6b
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-10-13 19:48:27 +0600

    Implements higher-order functions, demonstrating function parameters and return values. Also removes the dependency on the custom `mathlib` package along with related commented-out code. Includes examples of calling and using these higher-order functions.

--- basic.go
+++ basic.go
@@ -2,20 +2,45 @@ package main
 
 import (
 	"fmt"
-	"math-package/mathlib"
 )
+
+// custome package function
+// import (
+// 	"math-package/mathlib"
+// )
 
 // function for add
 
-func add(a int, b int) int {
-	return a + b
+// func add(a int, b int) int {
+// 	return a + b
+// }
+
+// func twoValueReturn(a int, b int) (int, int) {
+
+// 	add := a + b
+// 	mul := a * b
+// 	return add, mul
+// }
+
+//! higher order function
+//! function as a parameter
+//! function as a return
+
+// higher order function declare with function and paramerer
+
+func higherOrderFunction(a, b int, Op func(y int, z int)) {
+	Op(a, b)
 }
 
-func twoValueReturn(a int, b int) (int, int) {
+func add(a int, b int) {
+	fmt.Println(a + b)
+}
 
-	add := a + b
-	mul := a * b
-	return add, mul
+//! higher order function with return function
+
+func higherOrderFunctionWithReturn(a, b int, Op func(y int, z int)) func(y int, z int) {
+	Op(a, b)
+	return add
 }
 
 func main() {
@@ -38,18 +63,29 @@ func main() {
 	// fmt.Println(result)
 
 	// call second value return function
-	add, mul := twoValueReturn(5, 10)
-	fmt.Println("return two value sum value and multiple of value", add, mul)
+	// add, mul := twoValueReturn(5, 10)
+	// fmt.Println("return two value sum value and multiple of value", add, mul)
 
 	// call package function
-	result := mathlib.Add(5, 10)
+	// result := mathlib.Add(5, 10)
 
 	// IIFE Immediately Invoked Function Expression
-	func() {
-		a := 10
-		b := 20
-		fmt.Println("a + b", a+b)
-	}()
+	// func() {
+	// 	a := 10
+	// 	b := 20
+	// 	fmt.Println("a + b", a+b)
+	// }()
 
-	fmt.Println("package function call result", result)
+	// fmt.Println("package function call result", result)
+
+	//! higher order function call
+
+	// higherOrderFunction(5, 10, add)
+
+	//! higher order function with return
+	// output will be 15 , 3
+	res := higherOrderFunctionWithReturn(5, 10, add)
+
+	res(1, 2)
+
 }
